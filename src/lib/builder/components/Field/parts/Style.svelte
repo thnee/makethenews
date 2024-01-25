6c920f7b0a9b4fe690e59289602008e1ff420d52
Mattias Lindvall <mattias.lindvall@gmail.com>
2024-01-25 16:14:03 +0100

    Improve button style

--- src/lib/builder/components/Field/parts/Style.svelte
+++ src/lib/builder/components/Field/parts/Style.svelte
@@ -6,7 +6,13 @@
 
 {#each $options as style}
 	<button
-		class="border rounded px-2 py-1 mr-2"
+		class="
+			px-2 py-1 mr-2
+			border rounded
+			bg-neutral-700
+			border-neutral-600
+			hover:bg-neutral-600
+		"
 		on:click={() => {value.set(style.name);}}
 	>
 		{style.label}
