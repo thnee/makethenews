5b6abe838f393dd50fdd673d24fb44f8cfefa0dd
Mattias Lindvall <mattias.lindvall@gmail.com>
2024-01-27 15:09:26 +0100

    Update disclaimer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,12 @@
 
 <Builder />
 
-<div class="text-xs p-4">
-	Default background:
-	<a href="https://www.freepik.com/free-vector/digital-global-technology-banner-with-circuit-diagram_4298994.htm#page=4&query=news%20background&position=6&from_view=search&track=ais&uuid=941b2b05-ae9d-4317-b45f-a2e50c26f41f">Image by starline</a> on Freepik
+<div class="text-xs flex flex-col gap-2 ml-4">
+	<p>
+		This application does not store content that is created by users. Content that is created by users only exists on the user device and is discarded upon exit.
+	</p>
+	<p>
+		Default background:
+		<a href="https://www.freepik.com/free-vector/digital-global-technology-banner-with-circuit-diagram_4298994.htm#page=4&query=news%20background&position=6&from_view=search&track=ais&uuid=941b2b05-ae9d-4317-b45f-a2e50c26f41f">Image by starline</a> on Freepik
+	</p>
 </div>
