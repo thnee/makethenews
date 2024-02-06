6ecd4abc867418962fe07155a22e6c0ec4e123b9
Mattias Lindvall <mattias.lindvall@gmail.com>
2024-02-06 19:11:21 +0100

    Add more supported font sizes in unfonts config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 				families: [
 					{
 						name: "Archivo",
-						weights: [400, 500, 600, 700],
+						weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
 						styles: ["italic", "normal"],
 					},
 					{
