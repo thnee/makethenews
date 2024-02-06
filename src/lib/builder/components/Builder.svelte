c26bb68398cf0046fddb48e583f50097f8fb044d
Mattias Lindvall <mattias.lindvall@gmail.com>
2024-02-06 19:12:26 +0100

    Load various combinations of font sizes before canvas render
    And only make the canvas depend on fonts being loaded

--- src/lib/builder/components/Builder.svelte
+++ src/lib/builder/components/Builder.svelte
@@ -8,14 +8,15 @@
 
 	let fontsLoaded = false;
 
-	let fonts = [
-		"500 16px Archivo",
-		"200 16px Archivo",
-		"200 22px Archivo",
-		"600 22px Archivo",
-		"600 28px Archivo Narrow",
-		"100 22px Archivo Narrow",
-	];
+	let sizes = Array.from(new Array(40), (x, i) => i + 10);
+	let weights = Array.from(new Array(9), (x, i) => (i + 1) * 100);
+	let fonts = [];
+	weights.forEach((weight) => {
+		sizes.forEach((size) => {
+			fonts.push(`${weight} ${size}px Archivo`);
+			fonts.push(`${weight} ${size}px Archivo Narrow`);
+		});
+	});
 
 	Promise.all(
 		fonts.map((item) => {
@@ -26,46 +27,46 @@
 	});
 </script>
 
-{#if fontsLoaded}
-	<div class="flex">
-		<div class="flex-none p-4" style="width: 500px;">
-			<Field name="style" />
-			<div class="flex gap-6 w-full">
-				<div class="flex-1">
-					<Field name="width" />
-				</div>
-				<div class="flex-1">
-					<Field name="height" />
-				</div>
+<div class="flex">
+	<div class="flex-none p-4" style="width: 500px;">
+		<Field name="style" />
+		<div class="flex gap-6 w-full">
+			<div class="flex-1">
+				<Field name="width" />
 			</div>
-			<Field name="bgColor" />
-			<Field name="bgImage" />
-			<Field name="images" />
-			<Field name="place" />
-			<Field name="time" />
-			<Field name="headline" />
-			<Field name="body" />
-			<Field name="details" />
-		</div>
-
-		<div class="flex flex-col">
-			<div class="p-4">
-				<Canvas />
+			<div class="flex-1">
+				<Field name="height" />
 			</div>
-
-			<button
-				class="
-					btn
-					w-56 h-12
-					mx-auto p-4
-					text-lg
-					flex items-center
-				"
-				on:click={() => {builder.downloadCanvas();}}
-			>
-				<Icon src={Download} class="w-6 h-6 mr-10" />
-				Download
-			</button>
 		</div>
+		<Field name="bgColor" />
+		<Field name="bgImage" />
+		<Field name="images" />
+		<Field name="place" />
+		<Field name="time" />
+		<Field name="headline" />
+		<Field name="body" />
+		<Field name="details" />
 	</div>
-{/if}
+
+	<div class="flex flex-col">
+		<div class="p-4">
+			{#if fontsLoaded}
+				<Canvas />
+			{/if}
+		</div>
+
+		<button
+			class="
+				btn
+				w-56 h-12
+				mx-auto p-4
+				text-lg
+				flex items-center
+			"
+			on:click={() => {builder.downloadCanvas();}}
+		>
+			<Icon src={Download} class="w-6 h-6 mr-10" />
+			Download
+		</button>
+	</div>
+</div>
