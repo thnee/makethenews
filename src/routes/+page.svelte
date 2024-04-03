e20337d495c075a3056356407f9d82bf66a385d5
Mattias Lindvall <mattias.lindvall@gmail.com>
2024-04-04 01:16:42 +0200

    Update footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,20 @@
 	</p>
 	<p>
 		Default background:
-		<a href="https://www.freepik.com/free-vector/digital-global-technology-banner-with-circuit-diagram_4298994.htm#page=4&query=news%20background&position=6&from_view=search&track=ais&uuid=941b2b05-ae9d-4317-b45f-a2e50c26f41f">Image by starline</a> on Freepik
+		<a
+			class="text-sky-300 hover:underline"
+			href="https://www.freepik.com/free-vector/digital-global-technology-banner-with-circuit-diagram_4298994.htm#page=4&query=news%20background&position=6&from_view=search&track=ais&uuid=941b2b05-ae9d-4317-b45f-a2e50c26f41f">
+			Image by starline
+		</a>
+		on Freepik
+	</p>
+	<p>
+		Source code:
+		<a
+			class="text-sky-300 hover:underline"
+			href="https://github.com/thnee/makethenews"
+		>
+			makethenews on Github
+		</a>
 	</p>
 </div>
